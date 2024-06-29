c05b48351937e0bacdd3848281a31d32c627b2a0
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-06-29 15:10:10 +0200

    issues-3-29062024

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,38 +1,42 @@
-// Importa la función para conectar a la base de datos MongoDB desde el utilitario de mongoose.
 import { connectDB } from "@/utils/mongoose";
-// Importa el modelo de datos Fighter.
 import Fighter from "@/models/Fighter";
-// Importa el componente para mostrar la tarjeta de luchadores.
 import FightersCard from "@/components/fightersCard";
-
-
 
 // Función asíncrona para cargar los luchadores desde la base de datos.
 const loadFighters = async () => {
-  // Conecta a la base de datos.
-  connectDB();
-  // Realiza una consulta para obtener todos los documentos de la colección de luchadores.
-  const fighters = await Fighter.find();
-  // Devuelve la lista de luchadores.
-  return fighters;
+  try {
+    // Conecta a la base de datos.
+    await connectDB();
+    // Realiza una consulta para obtener todos los documentos de la colección de luchadores.
+    const fighters = await Fighter.find();
+    // Devuelve la lista de luchadores.
+    return fighters;
+  } catch (error) {
+    console.error('Error al cargar los luchadores:', error);
+    throw error; // Propaga el error para manejarlo donde sea necesario
+  }
 }
 
 // Componente de página principal que se renderiza de forma asíncrona.
 const Page = async () => {
-  // Carga los luchadores llamando a la función loadFighters.
-  const fighters = await loadFighters();
+  try {
+    // Carga los luchadores llamando a la función loadFighters.
+    const fighters = await loadFighters();
 
-  return (
-    <div className="text-secondary flex flex-col justify-center items-center">
-      {/* Mapea a través de la lista de luchadores y renderiza una tarjeta para cada uno */}
-      {fighters.map(fighter => {
-        return (
+    return (
+      <div className="text-secondary flex flex-col justify-center items-center">
+        {/* Mapea a través de la lista de luchadores y renderiza una tarjeta para cada uno */}
+        {fighters.map(fighter => (
           // Renderiza el componente FightersCard pasando el luchador como prop y utilizando el ID del luchador como clave.
           <FightersCard fighter={fighter} key={fighter._id}/>
-        )
-      })}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error('Error en la página principal:', error);
+    // Puedes manejar el error de renderización aquí, por ejemplo, mostrando un mensaje de error.
+    return <div>Ocurrió un error al cargar los luchadores.</div>;
+  }
 }
 
 export default Page;
